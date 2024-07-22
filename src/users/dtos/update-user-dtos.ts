1e76dcfed4df0cef99c073a9d22dc32693383085
= <=>
2024-07-21 21:27:17 -0400

    feat(users/dtos): implemented dtos for update user

---
+++ src/users/dtos/update-user-dtos.ts
@@ -0,0 +1,23 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  @ApiProperty({
+    description: 'Para atualizar um usuario deverá ser informado um nome.',
+    example: 'João silva',
+  })
+  name?: string;
+
+  @IsOptional()
+  @ApiPropertyOptional({
+    description: 'Para atualizar um usuario poderá ser informado uma senha.',
+    example: 'joao1234',
+  })
+  password?: string;
+
+  @IsOptional()
+  @IsString()
+  parentUserId: string;
+}
