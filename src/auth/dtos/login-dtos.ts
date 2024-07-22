a9ac39acca232fa8f222e6835839f1604b66c7aa
= <=>
2024-07-22 18:45:19 -0400

    fix(logindto): improving dtos login examples

--- src/auth/dtos/login-dtos.ts
+++ src/auth/dtos/login-dtos.ts
@@ -3,13 +3,13 @@ import { ApiProperty } from '@nestjs/swagger';
 export class LoginDto {
   @ApiProperty({
     description: 'Nome de usuário para login',
-    example: 'johndoe',
+    example: 'Maria silva',
   })
   username: string;
 
   @ApiProperty({
     description: 'Senha do usuário',
-    example: 'password123',
+    example: 'Maria1234',
   })
   password: string;
 }
