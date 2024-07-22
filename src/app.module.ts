212c21ecd52e8ae58ddcd0f8db76939d3ed36cc0
= <=>
2024-07-21 21:18:47 -0400

    fix(server): fix the database port

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,7 @@ dotenv.config();
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
       username: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
