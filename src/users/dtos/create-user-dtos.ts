7537ef55a67cba503efc83903578d95dad756831
= <=>
2024-07-21 21:22:26 -0400

    feat(users/dtos): implemented dtos for create users

---
+++ src/users/dtos/create-user-dtos.ts
@@ -0,0 +1,25 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreatUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'Para criar um usuario deverá ser informado um nome.',
+    example: 'João silva',
+  })
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'Para criarum usuario deverá ser informada uma senha',
+    example: 'joao1234',
+  })
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiPropertyOptional({})
+  parentUserId?: string;
+}
