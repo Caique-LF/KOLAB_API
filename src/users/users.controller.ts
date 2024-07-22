552c8069c5519cee31159d579c19ba5c39b66c95
= <=>
2024-07-21 21:42:15 -0400

    feat(users): implemented rout and service for create user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreatUserDto } from './dtos/create-user-dtos';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post()
+  async createUser(@Body() createUserDto: CreatUserDto) {
+    return this.userService.createUser(createUserDto);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/database/user.entity';
+import { Repository } from 'typeorm';
+import { CreatUserDto } from './dtos/create-user-dtos';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly UserRespository: Repository<User>,
+  ) {}
+
+  async createUser(createUserDto: CreatUserDto): Promise<any> {
+    const pass = await bcrypt.hash(createUserDto.password, 10);
+    createUserDto.password = pass;
+
+    const user = this.UserRespository.create({
+      ...createUserDto,
+      parentUserId: createUserDto.parentUserId
+        ? { id: createUserDto.parentUserId }
+        : null,
+    });
+
+    this.UserRespository.save(user);
+  }
+}
